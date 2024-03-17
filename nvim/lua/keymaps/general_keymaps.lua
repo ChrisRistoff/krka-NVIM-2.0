58da3db2ebdcd5580d0cb622c9b5188e1a50d7f4
Krasyo <doanusho@gmail.com>
2024-03-17 10:42:21 +0000

    remap movement keys to test

--- nvim/lua/keymaps/general_keymaps.lua
+++ nvim/lua/keymaps/general_keymaps.lua
@@ -26,3 +26,18 @@ vim.api.nvim_set_keymap('n', '<leader>rr', ':%s//<Left>',
 vim.api.nvim_set_keymap('n', '<A-w>', '<Esc>A;<Esc>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', 'dr', '0d$a', { noremap = true, silent = true })
+
+
+
+-- remap movement keys
+-- Enter insert mode with 'h'
+vim.api.nvim_set_keymap('n', 'h', 'i', {noremap = true})
+
+-- Move left with 'j'
+vim.api.nvim_set_keymap('n', 'j', 'h', {noremap = true})
+
+-- Move down with 'k'
+vim.api.nvim_set_keymap('n', 'k', 'j', {noremap = true})
+
+-- Move up with 'i'
+vim.api.nvim_set_keymap('n', 'i', 'k', {noremap = true})
